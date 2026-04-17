89f02b74e16ee2327e92bbb21b4a445cb5fc979d
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-04-17 17:52:59 +0100

    Initial commit

---
+++ Post.cs
@@ -0,0 +1,10 @@
+public class Post
+{
+    public required string Title { get; set; }
+
+    public required string Content { get; set; }
+
+    public required string Category { get; set; }
+
+    public required List<string> Tags { get; set; }
+}
