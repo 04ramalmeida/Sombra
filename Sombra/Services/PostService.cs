cbb0f985cf55c64ec1a55352a825a7174e1d703f
Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-07-16 00:31:08 +0100

    refactor: refactor PostService.cs for the tags rework

--- Sombra/Services/PostService.cs
+++ Sombra/Services/PostService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Sombra.Endpoints;
 using Sombra.Models.DTOs;
+using Sombra.Models.Entities;
 
 namespace Sombra.Services;
 
@@ -8,23 +9,47 @@ public class PostService(SombraDb db)
 {
     private readonly SombraDb _db = db;
 
-    internal async Task<List<Post>> GetPostsAsync(string? term)
+    internal async Task<List<PostResponseDto>> GetPostsAsync(string? term)
     {
         if (string.IsNullOrEmpty(term))
-        {
-            return await db.Posts.ToListAsync();
+        { //TODO: can probably find a way to not repeat this
+            return await db.Posts.Select(p => new PostResponseDto(
+                    p.Id,
+                    p.Title,
+                    p.Content,
+                    p.Category,
+                    new List<string>(p.Tags.Select(t => t.Name))
+                ))
+                .ToListAsync();
         }
         
         return await db.Posts.Where(p => p.Title.ToLower().Contains(term) ||
                                          p.Content.ToLower().Contains(term) ||
                                          p.Category.ToLower().Contains(term)
-        ).ToListAsync();
+        ).Select(p => new PostResponseDto(p.Id,
+                p.Title,
+                p.Content,
+                p.Category,
+                p.Tags.Select(t => t.Name).ToList()))
+            .ToListAsync();
+        
     }
 
-    internal async Task<Post?> GetPostAsync(int id)
+    internal async Task<PostResponseDto?> GetPostDtoAsync(int id)
+    {
+        return await db.Posts.Where(p => p.Id == id)
+            .Select(p => new PostResponseDto(
+                p.Id,
+                p.Title,
+                p.Content,
+                p.Category,
+                p.Tags.Select(t => t.Name).ToList())).FirstOrDefaultAsync();
+    }
+
+    internal async Task<Post?> GetPostByIdAsync(int id)
     {
         return await db.Posts.FindAsync(id);
-    }
+    } 
 
     internal async Task<Post> CreatePostAsync(Post post)
     {
@@ -33,12 +58,13 @@ public class PostService(SombraDb db)
         return post;
     }
 
-    internal async Task<Post> UpdatePostAsync(Post post, PostDto input)
+    internal async Task<Post> UpdatePostAsync(Post post,
+        CreatePostDto input)
     {
         post.Title = input.Title;
         post.Content = input.Content;
         post.Category = input.Category;
-        post.Tags = input.Tags.ToList();
+        post.Tags = GetOrCreateTags(input.Tags);
 
         await db.SaveChangesAsync();
         
@@ -49,5 +75,23 @@ public class PostService(SombraDb db)
     {
         db.Posts.Remove(post);
         await db.SaveChangesAsync();
+    }
+    
+    public List<Tag> GetOrCreateTags(List<string> tags)
+    {
+        List<Tag> result = [];
+        
+        result.AddRange(_db.Tags.Where(t => tags.Contains(t.Name)));
+        
+        List<Tag> newTags = 
+            tags.Except(result.Select(t => t.Name))
+                .Select(name => new Tag(name))
+                .ToList();
+        
+        _db.Tags.AddRange(newTags);
+        
+        result.AddRange(newTags);
+        
+        return result;
     }
 }
