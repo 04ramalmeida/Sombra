95a330015d8011df94d828abbb0cbb19db95a156
Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-07-16 00:30:29 +0100

    test: update test database seeding for the tags rework

--- Sombra.IntegrationTests/DbSeeder.cs
+++ Sombra.IntegrationTests/DbSeeder.cs
@@ -1,14 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using Sombra.Models.Entities;
 using Sombra.Utils;
 
 namespace Sombra.IntegrationTests;
 
 public class DbSeeder
 {
-    public static void CreatePosts(DbContext context)
+    public static void InitDb(DbContext context)
     {
-        if (context.Set<Post>().Any()) return;
-        context.Set<Post>().Add(PostUtils.ExamplePost());
+        if (context.Set<Post>().Any())
+        {
+            context.Set<Post>().RemoveRange(context.Set<Post>());
+        }
+
+        if (context.Set<Tag>().Any())
+        {
+            context.Set<Tag>().RemoveRange(context.Set<Tag>());
+        }
+
+        var examplePost = PostUtils.ExamplePostDto();
+        
+        var tag = new Tag(examplePost.Tags[0]);
+        context.Set<Tag>().Add(tag);
+
+        List<Post> posts = [];
+        
+        for (int i = 0; i < 2; i++)
+        {
+            posts.Add(new Post
+            {
+                Title = examplePost.Title,
+                Content = examplePost.Content,
+                Category =  examplePost.Category,
+                Tags = [tag]
+            });
+        }
+        
+        context.Set<Post>().AddRange(posts);
+        
+        
         context.SaveChanges();
     }
 }
