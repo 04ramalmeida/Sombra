c2a9b95ea46ddda617642599233f21a38f704ff8
Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-07-22 01:55:48 +0100

    feat: add sorting extension to posts

--- Sombra/Extensions/PostQueryExtensions.cs
+++ Sombra/Extensions/PostQueryExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Namotion.Reflection;
 
 namespace Sombra.Extensions;
 
@@ -13,5 +14,18 @@ public static class PostQueryExtensions
             || EF.Functions.Like(p.Category, $"%{searchTerm}%")
             || EF.Functions.Like(p.Category, $"%{searchTerm}%")
             || p.Tags.Any(t=> EF.Functions.Like(t.Name, $"%{searchTerm}%")));
+    }
+
+    public static IQueryable<Post> ApplySort(this IQueryable<Post> query, bool ascending, string sortBy = "title")
+    {
+        var sortProp = sortBy.ToLower();
+        
+        return sortProp switch
+        {
+            "id" => ascending ? query.OrderBy(p => p.Id) : query.OrderByDescending(p => p.Id),
+            "title" => ascending ? query.OrderBy(p => p.Title) : query.OrderByDescending(p => p.Title),
+            "category" => ascending ? query.OrderBy(p => p.Category) : query.OrderByDescending(p => p.Category),
+            _ => throw new ArgumentException($"Unknown sort property: {sortProp}")
+        };
     }
 }
