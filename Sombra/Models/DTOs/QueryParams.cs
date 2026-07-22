98fa3666369ff6ab693cef639250d92d9f1151b8
Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-07-22 01:53:18 +0100

    refactor: set default value of ascending param to be "true"

--- Sombra/Models/DTOs/QueryParams.cs
+++ Sombra/Models/DTOs/QueryParams.cs
@@ -5,7 +5,7 @@ public class QueryParams
     public string? SearchTerm {get; set;}
     
     public string? SortBy { get; set; }
-    public bool? Ascending { get; set; }
+    public bool? Ascending { get; set; } = true;
     
     public int? PageNumber { get; set; }
     public int? PageSize { get; set; }
