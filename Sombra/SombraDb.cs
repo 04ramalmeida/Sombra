fcd73bffd89aa1c0a858381f126b9e28286e9331
Almeida <176084410+04ramalmeida@users.noreply.github.com>
2026-08-03 18:21:42 +0100

    feat: db context will now set timestamps whenever changes are saved

--- Sombra/SombraDb.cs
+++ Sombra/SombraDb.cs
@@ -6,9 +6,42 @@ namespace Sombra;
 public class SombraDb : DbContext
 {
     public SombraDb(DbContextOptions<SombraDb> options)
-        : base(options) { }
-
+        : base(options)
+    {
+    }
+    
     public DbSet<Post> Posts => Set<Post>();
     
     public DbSet<Tag> Tags => Set<Tag>();
+
+    public override int SaveChanges()
+    {
+        SetTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SetTimestamps();
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+    
+    private void SetTimestamps()
+    {
+        foreach (var entity in ChangeTracker.Entries()
+                     .Where(e => e.State == EntityState.Added)
+                     .Select(e => e.Entity)
+                     .OfType<IEntity>())
+        {
+            entity.CreatedAt = DateTimeOffset.UtcNow;
+        }
+        
+        foreach (var entity in ChangeTracker.Entries()
+                     .Where(e => e.State == EntityState.Modified)
+                     .Select(e => e.Entity)
+                     .OfType<IEntity>())
+        {
+            entity.UpdatedAt = DateTimeOffset.UtcNow;
+        }
+    }
 }
